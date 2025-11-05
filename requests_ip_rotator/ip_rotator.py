d5eaea4e74f6bc1f4c8fd1c4b8c2a9df5ca9788e
Santiago Ramirez <miguelsantiago1940@gmail.com>
2025-11-05 11:08:58 -0400

    chore: region refactoring

--- requests_ip_rotator/ip_rotator.py
+++ requests_ip_rotator/ip_rotator.py
@@ -1,5 +1,6 @@
 import logging
 from time import sleep
+from typing import List
 import concurrent.futures
 from random import choice, randint
 
@@ -12,58 +13,12 @@ logger = logging.getLogger(__name__)
 
 MAX_IPV4 = ipaddress.IPv4Address._ALL_ONES
 
-NORTH_AMERICA = [
-    "us-east-1",
-    "us-east-2",
-    "us-west-1",
-    "us-west-2",
-    "ca-west-1",
-    "ca-central-1",
-    "mx-central-1",
-]
-
-EUROPE = [
-    "eu-west-1",
-    "eu-west-2",
-    "eu-west-3",
-    "eu-central-1",
-    "eu-central-2",
-    "eu-south-1",
-    "eu-south-2",
-    "eu-north-1",
-]
-
-ASIA_PACIFIC = [
-    "ap-south-1",
-    "ap-northeast-3",
-    "ap-northeast-2",
-    "ap-southeast-1",
-    "ap-southeast-2",
-    "ap-northeast-1",
-    "ap-east-1",
-]
-
-SOUTH_AMERICA = [
-    "sa-east-1",
-]
-
-AFRICA = [
-    "af-south-1",
-]
-
-MIDDLE_EAST = [
-    "me-south-1",
-    "me-central-1",
-    "il-central-1",
-]
-
-
 class ApiGateway(rq.adapters.HTTPAdapter):
 
     def __init__(
         self,
         site: str,
-        regions=NORTH_AMERICA,
+        regions: List[str],
         access_key_id=None,
         access_key_secret=None,
         **kwargs,
